05a43561e33982a81fd8a3f91df079c0f966d13f
Angel Trinh <trinh.angel@gmail.com>
2017-10-05 15:16:20 -0400

    hw

---
+++ hw6.py
@@ -0,0 +1,24 @@
+class Shapes:
+    pass
+
+square = Shapes()
+square.perimeter = 4
+square.area = 16
+
+print(square.perimeter, square.area)
+
+rectangle = Shapes()
+rectangle.perimeter = 8
+rectangle.area = 64
+print(rectangle.perimeter, rectangle.area)
+
+
+triangle = Shapes()
+triangle.perimeter = 6
+triangle.area = 36
+print(triangle.perimeter, triangle.area)
+
+circle = Shapes()
+circle.perimeter = 6.28
+circle.area = 3.14
+print(circle.perimeter, circle.area)
