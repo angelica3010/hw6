86da21e72063dc6888fb50005937c561098f3038
Angel Trinh <trinh.angel@gmail.com>
2017-10-12 03:50:57 -0400

    updated hw

--- hw6.py
+++ hw6.py
@@ -1,9 +1,14 @@
-class Shapes:
+#HW #6: Create a class called shapes. The properties should be sides. It should have the methods: perimeter, area
+
+#Create 4 different instances of the class. Print out each attribute of the instance and use each method in the class.
+
+'''class Shapes:
     pass
 
 square = Shapes()
 square.perimeter = 4
 square.area = 16
+print(f"permiter {square.perimeter}, {square.area}")
 
 print(square.perimeter, square.area)
 
@@ -21,4 +26,81 @@ print(triangle.perimeter, triangle.area)
 circle = Shapes()
 circle.perimeter = 6.28
 circle.area = 3.14
-print(circle.perimeter, circle.area)
+print(circle.perimeter, circle.area)'''
+
+#make a class
+class Shapes(object):
+#make the property aka attribute sides:
+    sides = None
+
+
+#make the method perimeter
+    def perimeter(self):
+        pass
+#make the method area
+    def area(self):
+        pass
+
+#instance of class Shapes
+
+
+class Circle(Shapes):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def perimeter(self):
+        return 2 * 3.14 * self.radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+class Rectangle(Shapes):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2 * self.length * self.width
+
+class Square(Shapes):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+    def perimeter(self):
+        return 2*self.length + 2* self.width
+
+class Triangle(Shapes):
+    def __init__(self, base, height, side1, side2):
+        self.base = base
+        self.height = height
+        self.side1 = side1
+        self.side2 = side2
+
+    def perimeter(self):
+        return self.base + self.side1 +self.side2
+
+    def area(self):
+        return self.base * self.height *.5
+
+circ = Circle(4)
+print(f"Circle's area is {circ.area()} and the perimeter is {circ.perimeter()}.")
+
+rect = Rectangle(4,6)
+print(f"Rectangle's area is {rect.area()} and the perimeter is {rect.perimeter()}.")
+
+sqr = Square(8,8)
+print(f"Square's area is {sqr.area()} and the perimeter is {sqr.perimeter()}.")
+
+tria = Triangle( 3, 3, 3, 3)
+rect = Rectangle(4,6)
+print(f"Triangle's area is {tria.area()} and the perimeter is {tria.perimeter()}.")
+
+
+#prints out the 15 most frequent from words.txt
